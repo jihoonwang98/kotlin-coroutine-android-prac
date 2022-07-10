706484a42274e0bf95d1063976d4f3b37fe74dd2
jihoonwang98 <jihoonwang98@naver.com>
2022-07-10 17:59:38 +0900

    implement feed summary

--- app/src/main/java/com/demo/MainActivity.kt
+++ app/src/main/java/com/demo/MainActivity.kt
@@ -2,7 +2,14 @@ package com.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.demo.adapter.ArticleAdapter
+import com.demo.model.Article
+import com.demo.model.Feed
 import kotlinx.coroutines.*
 import org.w3c.dom.Element
 import org.w3c.dom.Node
@@ -12,16 +19,28 @@ class MainActivity : AppCompatActivity() {
     private val dispatcher = newFixedThreadPoolContext(2, "IO")
     private val factory = DocumentBuilderFactory.newInstance()
 
+    private lateinit var articles: RecyclerView
+    private lateinit var viewAdapter: ArticleAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+
     private val feeds = listOf(
-        "https://www.npr.org/rss/rss.php?id=1001",
-        "http://rss.donga.com/total.xml",
-        "http://rss.hankooki.com/sports/sp00_list.xml",
-        "asdifo"
+        Feed("npr", "https://www.npr.org/rss/rss.php?id=1001"),
+        Feed("donga", "http://rss.donga.com/total.xml"),
+        Feed("hankook", "http://rss.hankooki.com/sports/sp00_list.xml"),
+        Feed("invalid", "asdifo")
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = ArticleAdapter()
+        articles = findViewById<RecyclerView>(R.id.articles).apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
 
         GlobalScope.launch(dispatcher) {
             asyncLoadNews()
@@ -29,31 +48,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun asyncLoadNews() = GlobalScope.launch {
-        val requests = mutableListOf<Deferred<List<String>>>()
-        feeds.mapTo(requests) { asyncFetchHeadlines(it, dispatcher) }
+        val requests = mutableListOf<Deferred<List<Article>>>()
+        feeds.mapTo(requests) { asyncFetchArticles(it, dispatcher) }
         requests.forEach { it.join() }
 
-        val headlines = requests
+        val articles = requests
             .filter { !it.isCancelled }
             .flatMap { it.getCompleted() }
 
         val failed = requests.filter { it.isCancelled }.size
-        val newsCount = findViewById<TextView>(R.id.newsCount)
-        val warnings = findViewById<TextView>(R.id.warnings)
         val obtained = requests.size - failed
 
         launch(Dispatchers.Main) {
-            newsCount.text = "Found ${headlines.size} News in ${obtained} feeds"
-
-            if (failed > 0) {
-                warnings.text = "Failed to fetch $failed feeds"
-            }
+            findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
+            viewAdapter.add(articles)
         }
     }
 
-    private fun asyncFetchHeadlines(feed: String, dispatcher: CoroutineDispatcher) = GlobalScope.async(dispatcher) {
+    private fun asyncFetchArticles(feed: Feed, dispatcher: CoroutineDispatcher) = GlobalScope.async(dispatcher) {
+        delay(1000)
+
         val builder = factory.newDocumentBuilder()
-        val xml = builder.parse(feed)
+        val xml = builder.parse(feed.url)
         val news = xml.getElementsByTagName("channel").item(0)
 
         (0 until news.childNodes.length)
@@ -63,7 +79,9 @@ class MainActivity : AppCompatActivity() {
             .map { it as Element }
             .filter { "item" == it.tagName }
             .map {
-                it.getElementsByTagName("title").item(0).textContent
+                val title = it.getElementsByTagName("title").item(0).textContent
+                val summary = it.getElementsByTagName("description").item(0).textContent
+                Article(feed.name, title, summary)
             }
             .toList()
     }
---
+++ app/src/main/java/com/demo/model/model.kt
@@ -0,0 +1,5 @@
+package com.demo.model
+
+data class Feed(val name: String, val url: String)
+
+data class Article(val feed: String, val title: String, val summary: String)
